d4fd315f3280465fd61355192f8f5e70ddf2ab9a
Bartłomiej Bukowski <bartlomiejbukowski.b@gmail.com>
2019-01-22 13:08:23 +0100

    [iOS][Android] Move document-picker to universal module

--- apps/native-component-list/screens/DocumentPickerScreen.js
+++ apps/native-component-list/screens/DocumentPickerScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Image, ScrollView, Text, View } from 'react-native';
+import { Alert, Image, Text, View, WebView } from 'react-native';
 import { DocumentPicker } from 'expo';
 import Button from '../components/Button';
 
@@ -7,10 +7,33 @@ export default class DocumentPickerScreen extends React.Component {
   static navigationOptions = {
     title: 'DocumentPicker',
   };
-  
+
   state = {
     document: null,
   };
+
+  get html() {
+    return '<!DOCTYPE html>\
+    <html lang="en">\
+    <head>\
+      <meta charset="UTF-8">\
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">\
+      <title>Document</title>\
+    </head>\
+    <body style="font: 1rem \'Fira Sans\', sans-serif; display: flex; justify-content: center; align-items: center; flex-direction: column;">\
+      <h2>Web View Document Picker</h2>\
+      <label\
+        for="input_file">Choose file:\
+      </label>\
+      <input\
+        type="file"\
+        id="input_file"\
+        accept="*/*"\
+      >\
+    </body>\
+    </html>';
+  }
 
   _openPicker = async () => {
     const result = await DocumentPicker.getDocumentAsync({});
@@ -45,9 +68,10 @@ export default class DocumentPickerScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, margin: 20 }}>
         <Button onPress={this._openPicker} title="Open document picker" />
         {this._renderDocument()}
+        <WebView style={{ flex: 1 }} source={{ html: this.html }} />
       </View>
     );
   }
